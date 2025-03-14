fb0a0443a5eb901c9d9bd61e437367c9bf2fcb71
KubaPro010 <kuba.pro010@gmail.com>
2025-03-14 22:11:33 +0100

    headers

--- src/lib.h
+++ src/lib.h
@@ -6,6 +6,6 @@ extern int ustrcmp(const unsigned char *s1, const unsigned char *s2);
 extern uint8_t get_rtp_tag_id(char *rtp_tag_name);
 extern char *get_rtp_tag_name(uint8_t rtp_tag);
 extern void add_checkwords(uint16_t *blocks, uint8_t *bits);
-extern uint8_t add_rds_af(struct rds_af_t *af_list, float freq);
-extern char *show_af_list(struct rds_af_t af_list);
+extern uint8_t add_rds_af(RDSAFs *af_list, float freq);
+extern char *show_af_list(RDSAFs af_list);
 extern unsigned char *xlat(unsigned char *str);
