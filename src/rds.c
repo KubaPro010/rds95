a59e066eacf64b738d1c29e37de53a26928c6e70
KubaPro010 <kuba.pro010@gmail.com>
2025-03-22 18:07:50 +0100

    oh

--- src/rds.c
+++ src/rds.c
@@ -741,7 +741,7 @@ void reset_rds_state(RDSEncoder* enc, uint8_t program) {
 	tempCoder.state[program].rt_ab = 1;
 	tempCoder.state[program].ptyn_ab = 1;
 	set_rds_rt1(&tempCoder, enc->data[program].rt1);
-	set_rds_rt1(&tempCoder, enc->data[program].rt2);
+	set_rds_rt2(&tempCoder, enc->data[program].rt2);
 	set_rds_ps(&tempCoder, enc->data[program].ps);
 	set_rds_dps1(&tempCoder, enc->data[program].dps1);
 	set_rds_tps(&tempCoder, enc->data[program].tps);
